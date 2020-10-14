64d92cf6688185abfed93b287b6516e65630e93a
Samuel Ogu <hello@samuelogu.com>
2020-10-14 12:16:57 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,14 +14,75 @@ let Person = {
             country: 'Nigeria',
             state: 'Akwa Ibom'+ ' state'
         }
-    }, "No. 12 Edet Akpan Avenue"]
+    }, "No. 12 Edet Akpan Avenue"],
+    todos: [
+        {
+            title: 'Fix phone screen',
+            completed: false
+        }
+    ],
+    search: {
+        location: '5m',
+    }
 }
 let names = [
 Person, "Edi"+'diong', "Ima", 6
 ]
 let about = person + " is " + age + " years old"
 // console.log(typeof(address))
-console.log(Person.addresses[0])
-console.log(names[0].addresses[0].phone[1])
-console.log(about)
-console.log(names[1])
+// console.log(Person.addresses[0])
+// console.log(names[0].addresses[0].phone[1])
+// console.log(about)
+// console.log(names[1])
+
+function sayHello(person, age) {
+    var greetings = 'Hello ' + person + '!'
+    var declearAge = person+'\'s age is '+age
+    console.log(greetings+' '+declearAge)
+}
+
+/*function calculateGrade(score) {
+    var grade = ''
+    if (score >= 70) {
+        grade = 'A'
+    }else if(score >= 60 && score < 70) {
+        grade = 'B'
+    }else if(score > 49 && score < 60) {
+        grade = 'C'
+    }else if(score >= 45 && score < 50) {
+        grade = 'D'
+    }else {
+        grade = 'F'
+    }
+    console.log(grade)
+}*/
+
+function calculateGrade(score) {
+
+    if (score >= 70) {
+        return 'A'
+    }
+
+    if(score >= 60 && score < 70) {
+        return 'B'
+    }
+
+    if(score > 49 && score < 60) {
+        return 'C'
+    }
+
+    if(score >= 45 && score < 50) {
+        return 'D'
+    }
+
+    return 'F'
+
+}
+
+// sayHello('James', 38)
+// sayHello(person, age)
+// sayHello('Blessing Philips', 25)
+let score = 67
+let isPass = calculateGrade(score) === 'F' ? 'Failed' : 'Pass'
+
+console.log(isPass)
