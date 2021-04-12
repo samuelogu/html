db789aa12aea57b55cfa7d9b610089ba52a5c289
Samuel Ogu <hello@samuelogu.com>
2021-04-12 22:08:55 +0100

    var file

---
+++ js/var.js
@@ -0,0 +1,17 @@
+var name = 'James Ogu'
+let email = 'psalm294u@gmail.com'
+const age = 30
+
+
+name = 'Samuel Ogu'
+
+console.log(name)
+console.log('My name is '+name);
+console.log(name+' is a male');
+//
+// variables
+// functions
+// arrays
+// loops
+// classes
+// conditional statements
